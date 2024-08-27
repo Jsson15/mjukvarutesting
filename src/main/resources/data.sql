41cf263b06d775abe8336fb87881c50dd9315bcb
Jsson15 <henric.k.johansson@telia.com>
2024-08-27 22:55:25 +0200

    changes a bit in sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO School (name) VALUES ('Springfield High School');
+INSERT INTO Student (first_name, last_name, birth_date, email, java_programming_grade, school_id)
+VALUES ('John', 'Doe', '2000-05-15', 'john.doe@example.com', 4.5, 1);
